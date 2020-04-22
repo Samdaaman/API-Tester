3bc4d4866deb2e9c9ca946122a1b764cecacafa1
Sam-Laptop-Git <>
2020-04-22 18:53:43 +1200

    updated ip

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import api_tests
 # Settings
 api_tests.set_base_url_baseline('http://csse-s365.canterbury.ac.nz:4001/api/v1')  # url of baseline server
 # api_tests.set_base_url_test('http://127.0.0.1:4200/api/v1')  # url of server to test against the baseline
-api_tests.set_base_url_test('http://csse-s365.canterbury.ac.nz:4120/api/v1')
+api_tests.set_base_url_test('http://csse-s365.canterbury.ac.nz:4130/api/v1')
 continue_on_failure = True
-log_success = False
+log_success = True
 
 # Getting Tests
 api_tests_to_run = api_tests.all_tests
